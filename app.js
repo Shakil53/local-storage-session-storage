58f52fccf30a08e6e342d31be151a7f77296bf84
Shakil53 <shakilahmed.dcc@gmail.com>
2022-11-15 12:07:23 +0600

    done

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const displayLocalStorageCart = () => {
+    const cart = getCart();
+    for (const name in cart) {
+        displayProduct(name);
+    }
+}
+
+
 const addItem = () => {
     const namefield = document.getElementById('input-field');
     const name = namefield.value;
@@ -22,7 +30,7 @@ const displayProduct = name => {
     ul.appendChild(li);
 }
 
-const getcart = () => {
+const getCart = () => {
     const cart = localStorage.getItem('cart');
     let cartObj;
     if (cart){
@@ -35,9 +43,20 @@ const getcart = () => {
     return cartObj;
 }
 const addProductToCart = name => {
-    const cart = getcart();
-    cart[name] = 1;
+    const cart = getCart();
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    }
+    else {
+        cart[name] = 1;
+    }
     console.log(cart);
     const cartStringified = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringified);
 }
+const purchase = () => {
+    document.getElementById('addProducts').textContent = '';
+    localStorage.removeItem('cart');
+
+}
+displayLocalStorageCart();
