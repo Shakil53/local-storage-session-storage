c77c8ce92a4d152d05754fffc15a8dd72249e37f
Shakil53 <shakilahmed.dcc@gmail.com>
2022-11-15 11:26:40 +0600

    new

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const addItem = () => {
     const namefield = document.getElementById('input-field');
     const name1 = namefield.value;
+    if (!name1) {
+        return;
+    }
 
-
+    // display ui
     displayProduct(name1);
+
+    // add to local storage
+    addProductToCart(name)
 
     namefield.value = '';
 
@@ -14,4 +20,24 @@ const displayProduct = name2 => {
     const li = document.createElement('li');
     li.innerText = name2;
     ul.appendChild(li);
+}
+
+const getcart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    if (cart){
+        cartObj = JSON.parse(cart);
+    }
+    else {
+        cartObj = {};
+
+    }
+    return cartObj;
+}
+const addProductToCart = name => {
+    const cart = getcart();
+    cart[name] = 1;
+    console.log(cart);
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
 }
